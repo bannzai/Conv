39f540bc221175713be969167a060b998586fcc8
bannzai <kingkong999yhirose@gmail.com>
2018-04-25 21:34:54 +0900

    :metal: add example project

---
+++ ColkExample/ColkExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColkExample
+//
+//  Created by Yudai.Hirose on 2018/04/25.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
