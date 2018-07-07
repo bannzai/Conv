d015f4cd28c58448388b0d01e815302574323476
bannzai <kingkong999yhirose@gmail.com>
2018-07-07 20:44:01 +0900

    :metal: overwrite section from Suift

--- ColkExample/Colk/Section.swift
+++ ColkExample/Colk/Section.swift
@@ -8,10 +8,51 @@
 
 import Foundation
 
-public protocol Section {
-    var items: [Item] { get set }
+public protocol CollectionViewSectionType {
+    var items: [CollectionViewItemType] { get set }
+    
+    var header: CollectionViewSectionHeaderFooterViewable? { get }
+    var footer: CollectionViewSectionHeaderFooterViewable? { get }
+    
+    mutating func remove(for item: Int) -> CollectionViewItemType
+    mutating func insert(_ item: CollectionViewItemType, to index: Int)
 }
 
-public struct SectionImpl: Section {
-    public var items: [Item]
+public protocol CollectionViewSectionDelegatable {
+    func inset(collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int) -> UIEdgeInsets?
+    func minimumLineSpacing(collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int) -> CGFloat?
+    func minimumInteritemSpacing(collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int) -> CGFloat?
+}
+
+public struct CollectionViewSection: CollectionViewSectionType {
+    public typealias SectionArgument = (Section: CollectionViewSection, collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int)
+    public var items: [CollectionViewItemType]
+    
+    public var header: CollectionViewSectionHeaderFooterViewable?
+    public var footer: CollectionViewSectionHeaderFooterViewable?
+    
+    public var inset: ((SectionArgument) -> UIEdgeInsets)?
+    public var minimumLineSpacing: ((SectionArgument) -> CGFloat)?
+    public var minimumInteritemSpacing: ((SectionArgument) -> CGFloat)?
+    
+    public mutating func remove(for item: Int) -> CollectionViewItemType {
+        return items.remove(at: item)
+    }
+    
+    public mutating func insert(_ item: CollectionViewItemType, to index: Int) {
+        items.insert(item, at: index)
+    }
+}
+
+
+extension CollectionViewSection: CollectionViewSectionDelegatable {
+    public func inset(collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int) -> UIEdgeInsets? {
+        return inset?((self, collectionView, collectionViewLayout, section))
+    }
+    public func minimumLineSpacing(collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int) -> CGFloat? {
+        return minimumLineSpacing?((self, collectionView, collectionViewLayout, section))
+    }
+    public func minimumInteritemSpacing(collectionView: UICollectionView, collectionViewLayout: UICollectionViewLayout, section: Int) -> CGFloat? {
+        return minimumInteritemSpacing?((self, collectionView, collectionViewLayout, section))
+    }
 }
