000945131e5dfb822a73f2de0b48b034c2904527
bannzai <kingkong999yhirose@gmail.com>
2018-07-20 10:50:35 +0900

    :metal: Add Reusable

---
+++ Colk/Reusable.swift
@@ -0,0 +1,15 @@
+//
+//  Reusable.swift
+//  Colk
+//
+//  Created by Yudai.Hirose on 2018/07/20.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public protocol Reusable {
+    var reusableIdentifier: String { get }
+    
+    func register(to collectionView: UICollectionView)
+}
