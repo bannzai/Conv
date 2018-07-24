e6bfaa7251fed8e71645571e7385ed93c2dece55
bannzai <kingkong999yhirose@gmail.com>
2018-07-24 19:04:33 +0900

    :construction: Implement Detail ViewController

--- ColkExample/ColkExample/SecondViewController.swift
+++ ColkExample/ColkExample/SecondViewController.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 public class SecondViewController: UIViewController {
+    private let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
+    public override func loadView() {
+        view = UIView(frame: UIScreen.main.bounds)
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            ]
+        )
+        
+        collectionView
+            .colk()
+            .create { (section) in
+                
+        }
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionView.reloadData()
+    }
 }
