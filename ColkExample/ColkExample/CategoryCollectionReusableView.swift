94ed09e7ecec2681971131eace8926217eccc10b
bannzai <kingkong999yhirose@gmail.com>
2018-07-24 16:40:40 +0900

    :art: Add name label

--- ColkExample/ColkExample/CategoryCollectionReusableView.swift
+++ ColkExample/ColkExample/CategoryCollectionReusableView.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class CategoryCollectionReusableView: UICollectionReusableView {
 
+    @IBOutlet weak var nameLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-    
 }
