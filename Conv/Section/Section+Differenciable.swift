363b8ab43fd2da027fa776cf62274f472690cfdb
bannzai <kingkong999yhirose@gmail.com>
2018-08-05 04:42:50 +0900

    :metal: Add Section+Differenciable

---
+++ Conv/Section/Section+Differenciable.swift
@@ -0,0 +1,13 @@
+//
+//  Section+Differenciable.swift
+//  Conv
+//
+//  Created by Yudai.Hirose on 2018/08/05.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+//extension Section: Differenciable {
+//
+//}
