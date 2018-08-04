c4ab8d4f323f18089a1594f9cec105b04acaf5a3
bannzai <kingkong999yhirose@gmail.com>
2018-08-05 04:43:05 +0900

    Revert ":recycle: Fix FakeDifference"
    This reverts commit a0184ccaa6dac8e1aaf4d82c8d0172d45a9f357e.

--- Conv/Algorithm/Differenciable+Internal/InternalDifferenciable.swift
+++ Conv/Algorithm/Differenciable+Internal/InternalDifferenciable.swift
@@ -8,24 +8,12 @@
 
 import Foundation
 
-fileprivate var callCount = 0
 internal struct FakeDifference: Differenciable {
-    
-    init() {
-        callCount += 1
-    }
-    
     var differenceIdentifier: String {
-        return description
+        return "FakeDifference"
     }
     
     func isEqual(to compare: Differenciable) -> Bool {
         return false
-    }
-}
-
-extension FakeDifference: CustomStringConvertible {
-    var description: String {
-        return "FakeDifference: \(callCount)"
     }
 }
