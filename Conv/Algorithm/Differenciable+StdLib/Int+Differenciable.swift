093aa39e16daeef9a82b6e5485ca11ed9268e496
bannzai <kingkong999yhirose@gmail.com>
2018-08-05 04:13:58 +0900

    :metal: Conform to Differenciable for Int

---
+++ Conv/Algorithm/Differenciable+StdLib/Int+Differenciable.swift
@@ -0,0 +1,11 @@
+//
+//  Int+Differenciable.swift
+//  Conv
+//
+//  Created by Yudai.Hirose on 2018/08/05.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+extension Int: Differenciable { }
