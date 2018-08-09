59ecdea69aa4abf206fdd4f594c18e917a3b277c
bannzai <kingkong999yhirose@gmail.com>
2018-08-09 22:56:10 +0900

    :metal: Add Interaction Collection view controller

---
+++ ConvExample/ConvExample/Interaction/InteractionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  InteractionCollectionViewCell.swift
+//  ConvExample
+//
+//  Created by Yudai.Hirose on 2018/08/09.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class InteractionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ConvExample/ConvExample/Interaction/InteractionCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InteractionCollectionViewController.swift
+//  ConvExample
+//
+//  Created by Yudai.Hirose on 2018/08/09.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
