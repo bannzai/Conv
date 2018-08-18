316715ad0f923d6a67f68be261277583f068eb8e
bannzai <kingkong999yhirose@gmail.com>
2018-08-18 13:15:05 +0900

    :metal: Add test for check count

--- ConvTests/ConvTests.swift
+++ ConvTests/ConvTests.swift
@@ -13,24 +13,24 @@ class ConvTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCreateSection() {
+        XCTContext.runActivity(named: "create section") { (activity) in
+            let conv = Conv()
+            XCTAssert(conv.sections.count == 0)
+            conv.create { (section) in return }
+            XCTAssert(conv.sections.count == 1)
+        }
+        XCTContext.runActivity(named: "create two sections") { (activity) in
+            let conv = Conv()
+            XCTAssert(conv.sections.count == 0)
+            conv.create(for: [make(0), make(1)], sections: { (element, section) in return })
+            XCTAssert(conv.sections.count == 2)
         }
     }
-    
 }
