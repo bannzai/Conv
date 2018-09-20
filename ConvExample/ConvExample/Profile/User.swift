91d11187f8b62755a731ed1d9185c8df6ecc9bbd
bannzai <kingkong999yhirose@gmail.com>
2018-09-20 11:04:40 +0900

    :sparkles: Add User model

---
+++ ConvExample/ConvExample/Profile/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  ConvExample
+//
+//  Created by Yudai.Hirose on 2018/09/20.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    let headerImage = #imageLiteral(resourceName: "conv_logo")
+    let profileImage = #imageLiteral(resourceName: "bannzai")
+    let name = "bannzai"
+    let introduction = """
+                            Hi, I'm bannzai.
+                            bannzai means \\(^o^)/.
+                            Nice to meet you.
+                            If you love this library, you should send start request to this repository.
+                            ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+                            So, I'm glad. I'm happy!!
+                            """
+}
+
