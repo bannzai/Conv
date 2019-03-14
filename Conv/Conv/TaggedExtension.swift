f4916136cb2c59a1ed42865d43462959504e857f
bannzai <kingkong999yhirose@gmail.com>
2019-03-14 14:36:17 +0900

    :sparkles: Add type of tagged extension

---
+++ Conv/Conv/TaggedExtension.swift
@@ -0,0 +1,32 @@
+//
+//  TaggedExtension.swift
+//  Conv
+//
+//  Created by Yudai.Hirose on 2019/03/14.
+//  Copyright © 2019 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public struct ConvTaggedExtension<Base> {
+    let base: Base
+    init (_ base: Base) {
+        self.base = base
+    }
+}
+
+public protocol ConvTaggedExtensionCompatible {
+    associatedtype Compatible
+    static var conv: ConvTaggedExtension<Compatible>.Type { get }
+    var conv: ConvTaggedExtension<Compatible> { get }
+}
+
+public extension ConvTaggedExtensionCompatible {
+    public static var conv: ConvTaggedExtension<Self>.Type {
+        return ConvTaggedExtension<Self>.self
+    }
+    
+    public var conv: ConvTaggedExtension<Self> {
+        return ConvTaggedExtension(self)
+    }
+}
